1d34c5de66c0e2a80613eaa90a0e9361c36c3941
Safat <ahamadahasaf@gmail.com>
2025-05-24 11:22:34 +0600

    CSRF token methord change

--- app/views/Profile/editProfile.blade.php
+++ app/views/Profile/editProfile.blade.php
@@ -87,7 +87,7 @@
     $(document).ready(function() {
         $.ajaxSetup({
             headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                'X-CSRF-TOKEN': '{{ csrf_token() }}'
             }
         });
         $(document).on('click', '#editProfile', function(e) {
--- app/views/User/slideCreate.blade.php
+++ app/views/User/slideCreate.blade.php
@@ -162,7 +162,7 @@
     $(document).ready(function() {
         $.ajaxSetup({
             headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                'X-CSRF-TOKEN': '{{ csrf_token() }}'
             }
         });
         $(document).on('keyup', '#password, #confirmPassword', function() {
